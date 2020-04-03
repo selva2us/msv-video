2c056f305becc6b87ad3ad56f7bff0677f4943bb
Selvam <selva2us@users.noreply.github.com>
2020-04-03 11:36:22 +0530

    port changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
-
+//const port = 3000;
+const PORT = process.env.PORT || 3000;
 // Set public folder as root
 app.use(express.static('public'));
 
